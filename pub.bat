bdf6301bae411ee67d766b803c288a77d069ade5
Jordi Giral <jgiral@metacodi.com>
2022-02-09 00:33:57 +0100

    published

--- pub.bat
+++ pub.bat
@@ -2,10 +2,10 @@ echo off
 cls
 
 if [%1]==[] (
+  echo "npx ts-node precode\publish.ts"
+  npx ts-node precode\publish.ts
+) else (
   REM Descripció pel commit.
   echo "npx ts-node precode\publish.ts -c %1"
   npx ts-node precode\publish.ts -c %1
-) else (
-  echo "npx ts-node precode\publish.ts"
-  npx ts-node precode\publish.ts
 )
