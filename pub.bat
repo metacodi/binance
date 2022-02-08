c107c904b1b3eb6c8b76099478d2ee65574a0557
Jordi Giral <jgiral@metacodi.com>
2022-02-09 00:47:15 +0100

    publicat amb una descripció pel commit

--- pub.bat
+++ pub.bat
@@ -5,7 +5,7 @@ if [%1]==[] (
   echo "npx ts-node precode\publish.ts"
   npx ts-node precode\publish.ts
 ) else (
-  REM Descripció pel commit.
-  echo "npx ts-node precode\publish.ts -c %1"
+  @REM Descripció pel commit.
+  @REM echo "npx ts-node precode\publish.ts -c %1 %2 %3 %4 %5 %6 %7 %8 %9"
   npx ts-node precode\publish.ts -v -c \"%1 %2 %3 %4 %5 %6 %7 %8 %9\"
 )
