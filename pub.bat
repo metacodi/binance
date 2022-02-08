f15601e984f46577d21fb36ca0ba79973d13ff94
Jordi Giral <jgiral@metacodi.com>
2022-02-09 00:33:24 +0100

    auto-commit

--- pub.bat
+++ pub.bat
@@ -1,1 +1,11 @@
-npx ts-node precode\publish.ts
+echo off
+cls
+
+if [%1]==[] (
+  REM Descripció pel commit.
+  echo "npx ts-node precode\publish.ts -c %1"
+  npx ts-node precode\publish.ts -c %1
+) else (
+  echo "npx ts-node precode\publish.ts"
+  npx ts-node precode\publish.ts
+)
