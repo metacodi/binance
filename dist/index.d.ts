12c4699adcee2b2cbf529a6759deafa5959e07a7
Jordi Giral <jgiral@metacodi.com>
2022-02-09 09:09:04 +0100

    auto-commit

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export * from './types/binance.types';
-export * from './types/binance-spot.types';
-export * from './types/binance-futures.types';
-export * from './types/binance-websocket.types';
-export * from './binance-api';
-export * from './binance-api-spot';
-export * from './binance-api-futures';
-export * from './binance-websocket';
-//# sourceMappingURL=index.d.ts.map
--- dist/types/binance-futures.types.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=binance-futures.types.js.map
