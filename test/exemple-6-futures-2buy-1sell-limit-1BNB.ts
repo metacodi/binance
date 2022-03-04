87012141f13a6892100cdb601ead34fb167885b8
xgiral-metacodi <65396010+xgiral-metacodi@users.noreply.github.com>
2022-03-04 10:25:15 +0100

    Update exemple-6-futures-2buy-1sell-limit-1BNB.ts

--- test/exemple-6-futures-2buy-1sell-limit-1BNB.ts
+++ test/exemple-6-futures-2buy-1sell-limit-1BNB.ts
@@ -402,3 +402,315 @@ const exemple_6_orderUpdate_ = {
  }
 };
 
+const exemple_6_orderUpdate_ = {
+ "eventType": "ORDER_TRADE_UPDATE",
+ "eventTime": 1646385216358,
+ "transactionTime": 1646385216352,
+ "order": {
+  "symbol": "BNBUSDT",
+  "clientOrderId": "web_9UbruVReJXAgaQFsrsqC",
+  "orderSide": "SELL",
+  "orderType": "LIMIT",
+  "orderTimeInForce": "GTC",
+  "originalQuantity": 0.15,
+  "originalPrice": 396.8,
+  "averagePrice": 0,
+  "stopPrice": 0,
+  "executionType": "NEW",
+  "orderStatus": "NEW",
+  "orderId": 37877420721,
+  "lastFilledQuantity": 0,
+  "orderFilledAccumulatedQuantity": 0,
+  "lastFilledPrice": 0,
+  "commissionAmount": null,
+  "orderTradeTime": 1646385216352,
+  "tradeId": 0,
+  "bidsNotional": 0,
+  "asksNotional": 59.52,
+  "isMakerTrade": false,
+  "isReduceOnly": false,
+  "stopPriceWorkingType": "CONTRACT_PRICE",
+  "originalOrderType": "LIMIT",
+  "positionSide": "BOTH",
+  "isCloseAll": false,
+  "trailingStopActivationPrice": null,
+  "trailingStopCallbackRate": null,
+  "realisedProfit": 0
+ }
+};
+
+const exemple_6_orderUpdate_ = {
+ "eventType": "ORDER_TRADE_UPDATE",
+ "eventTime": 1646385512006,
+ "transactionTime": 1646385511999,
+ "order": {
+  "symbol": "BNBUSDT",
+  "clientOrderId": "web_RLlsI8MD6APf691BUbyr",
+  "orderSide": "BUY",
+  "orderType": "MARKET",
+  "orderTimeInForce": "GTC",
+  "originalQuantity": 0.13,
+  "originalPrice": 0,
+  "averagePrice": 0,
+  "stopPrice": 0,
+  "executionType": "NEW",
+  "orderStatus": "NEW",
+  "orderId": 37877489746,
+  "lastFilledQuantity": 0,
+  "orderFilledAccumulatedQuantity": 0,
+  "lastFilledPrice": 0,
+  "commissionAmount": null,
+  "orderTradeTime": 1646385511999,
+  "tradeId": 0,
+  "bidsNotional": 0,
+  "asksNotional": 59.52,
+  "isMakerTrade": false,
+  "isReduceOnly": false,
+  "stopPriceWorkingType": "CONTRACT_PRICE",
+  "originalOrderType": "MARKET",
+  "positionSide": "BOTH",
+  "isCloseAll": false,
+  "trailingStopActivationPrice": null,
+  "trailingStopCallbackRate": null,
+  "realisedProfit": 0
+ }
+};
+
+const exemple_6_balanceUpdate_ = {
+ "eventType": "ACCOUNT_UPDATE",
+ "eventTime": 1646385512006,
+ "transactionId": 1646385511999,
+ "updateData": {
+  "updateEventType": "ORDER",
+  "updatedBalances": [
+   {
+    "asset": "USDT",
+    "balanceChange": 0,
+    "crossWalletBalance": 27.99633953,
+    "walletBalance": 27.99633953
+   }
+  ],
+  "updatedPositions": [
+   {
+    "symbol": "BNBUSDT",
+    "positionAmount": 0.14,
+    "entryPrice": 394.94786,
+    "accumulatedRealisedPreFee": 0.6052,
+    "unrealisedPnl": -0.003899,
+    "marginType": "cross",
+    "isolatedWalletAmount": 0,
+    "positionSide": "BOTH"
+   }
+  ]
+ }
+};
+
+const exemple_6_orderUpdate_ = {
+ "eventType": "ORDER_TRADE_UPDATE",
+ "eventTime": 1646385512006,
+ "transactionTime": 1646385511999,
+ "order": {
+  "symbol": "BNBUSDT",
+  "clientOrderId": "web_RLlsI8MD6APf691BUbyr",
+  "orderSide": "BUY",
+  "orderType": "MARKET",
+  "orderTimeInForce": "GTC",
+  "originalQuantity": 0.13,
+  "originalPrice": 0,
+  "averagePrice": 394.89,
+  "stopPrice": 0,
+  "executionType": "TRADE",
+  "orderStatus": "FILLED",
+  "orderId": 37877489746,
+  "lastFilledQuantity": 0.13,
+  "orderFilledAccumulatedQuantity": 0.13,
+  "lastFilledPrice": 394.89,
+  "commissionAsset": "USDT",
+  "commissionAmount": 0.02053427,
+  "orderTradeTime": 1646385511999,
+  "tradeId": 649730862,
+  "bidsNotional": 0,
+  "asksNotional": 59.52,
+  "isMakerTrade": false,
+  "isReduceOnly": false,
+  "stopPriceWorkingType": "CONTRACT_PRICE",
+  "originalOrderType": "MARKET",
+  "positionSide": "BOTH",
+  "isCloseAll": false,
+  "trailingStopActivationPrice": null,
+  "trailingStopCallbackRate": null,
+  "realisedProfit": 0
+ }
+};
+
+const exemple_6_orderUpdate_ = {
+ "eventType": "ORDER_TRADE_UPDATE",
+ "eventTime": 1646385537284,
+ "transactionTime": 1646385537278,
+ "order": {
+  "symbol": "BNBUSDT",
+  "clientOrderId": "web_FUhUkLbJS1PoYwvi8hNH",
+  "orderSide": "SELL",
+  "orderType": "LIMIT",
+  "orderTimeInForce": "GTC",
+  "originalQuantity": 0.12,
+  "originalPrice": 396.8,
+  "averagePrice": 0,
+  "stopPrice": 0,
+  "executionType": "NEW",
+  "orderStatus": "NEW",
+  "orderId": 37877495891,
+  "lastFilledQuantity": 0,
+  "orderFilledAccumulatedQuantity": 0,
+  "lastFilledPrice": 0,
+  "commissionAmount": null,
+  "orderTradeTime": 1646385537278,
+  "tradeId": 0,
+  "bidsNotional": 0,
+  "asksNotional": 107.136,
+  "isMakerTrade": false,
+  "isReduceOnly": false,
+  "stopPriceWorkingType": "CONTRACT_PRICE",
+  "originalOrderType": "LIMIT",
+  "positionSide": "BOTH",
+  "isCloseAll": false,
+  "trailingStopActivationPrice": null,
+  "trailingStopCallbackRate": null,
+  "realisedProfit": 0
+ }
+};
+
+const exemple_6_balanceUpdate_ = {
+ "eventType": "ACCOUNT_UPDATE",
+ "eventTime": 1646385874517,
+ "transactionId": 1646385874489,
+ "updateData": {
+  "updateEventType": "ORDER",
+  "updatedBalances": [
+   {
+    "asset": "USDT",
+    "balanceChange": 0,
+    "crossWalletBalance": 28.24373552,
+    "walletBalance": 28.24373552
+   }
+  ],
+  "updatedPositions": [
+   {
+    "symbol": "BNBUSDT",
+    "positionAmount": -0.01,
+    "entryPrice": 396.8,
+    "accumulatedRealisedPreFee": 0.86449999,
+    "unrealisedPnl": 0.00076352,
+    "marginType": "cross",
+    "isolatedWalletAmount": 0,
+    "positionSide": "BOTH"
+   }
+  ]
+ }
+};
+
+const exemple_6_orderUpdate_ = {
+ "eventType": "ORDER_TRADE_UPDATE",
+ "eventTime": 1646385874517,
+ "transactionTime": 1646385874489,
+ "order": {
+  "symbol": "BNBUSDT",
+  "clientOrderId": "web_9UbruVReJXAgaQFsrsqC",
+  "orderSide": "SELL",
+  "orderType": "LIMIT",
+  "orderTimeInForce": "GTC",
+  "originalQuantity": 0.15,
+  "originalPrice": 396.8,
+  "averagePrice": 396.8,
+  "stopPrice": 0,
+  "executionType": "TRADE",
+  "orderStatus": "FILLED",
+  "orderId": 37877420721,
+  "lastFilledQuantity": 0.15,
+  "orderFilledAccumulatedQuantity": 0.15,
+  "lastFilledPrice": 396.8,
+  "commissionAsset": "USDT",
+  "commissionAmount": 0.011904,
+  "orderTradeTime": 1646385874489,
+  "tradeId": 649733423,
+  "bidsNotional": 0,
+  "asksNotional": 0,
+  "isMakerTrade": true,
+  "isReduceOnly": false,
+  "stopPriceWorkingType": "CONTRACT_PRICE",
+  "originalOrderType": "LIMIT",
+  "positionSide": "BOTH",
+  "isCloseAll": false,
+  "trailingStopActivationPrice": null,
+  "trailingStopCallbackRate": null,
+  "realisedProfit": 0.25929999
+ }
+};
+
+const exemple_6_balanceUpdate_ = {
+ "eventType": "ACCOUNT_UPDATE",
+ "eventTime": 1646385874517,
+ "transactionId": 1646385874489,
+ "updateData": {
+  "updateEventType": "ORDER",
+  "updatedBalances": [
+   {
+    "asset": "USDT",
+    "balanceChange": 0,
+    "crossWalletBalance": 28.23421232,
+    "walletBalance": 28.23421232
+   }
+  ],
+  "updatedPositions": [
+   {
+    "symbol": "BNBUSDT",
+    "positionAmount": -0.13,
+    "entryPrice": 396.8,
+    "accumulatedRealisedPreFee": 0.86449999,
+    "unrealisedPnl": 0.00992576,
+    "marginType": "cross",
+    "isolatedWalletAmount": 0,
+    "positionSide": "BOTH"
+   }
+  ]
+ }
+};
+
+const exemple_6_orderUpdate_ = {
+ "eventType": "ORDER_TRADE_UPDATE",
+ "eventTime": 1646385874517,
+ "transactionTime": 1646385874489,
+ "order": {
+  "symbol": "BNBUSDT",
+  "clientOrderId": "web_FUhUkLbJS1PoYwvi8hNH",
+  "orderSide": "SELL",
+  "orderType": "LIMIT",
+  "orderTimeInForce": "GTC",
+  "originalQuantity": 0.12,
+  "originalPrice": 396.8,
+  "averagePrice": 396.8,
+  "stopPrice": 0,
+  "executionType": "TRADE",
+  "orderStatus": "FILLED",
+  "orderId": 37877495891,
+  "lastFilledQuantity": 0.12,
+  "orderFilledAccumulatedQuantity": 0.12,
+  "lastFilledPrice": 396.8,
+  "commissionAsset": "USDT",
+  "commissionAmount": 0.0095232,
+  "orderTradeTime": 1646385874489,
+  "tradeId": 649733425,
+  "bidsNotional": 0,
+  "asksNotional": 0,
+  "isMakerTrade": true,
+  "isReduceOnly": false,
+  "stopPriceWorkingType": "CONTRACT_PRICE",
+  "originalOrderType": "LIMIT",
+  "positionSide": "BOTH",
+  "isCloseAll": false,
+  "trailingStopActivationPrice": null,
+  "trailingStopCallbackRate": null,
+  "realisedProfit": 0
+ }
+};
+
